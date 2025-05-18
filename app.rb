bd69a5442d12e50fb0abef618385dff3589d9217
baileydodds <ddodds@chicagobooth.edu>
2025-05-18 17:13:51 +0000

    3

--- app.rb
+++ app.rb
@@ -2,8 +2,54 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+get("/rock") do 
+  moves = ["rock", "paper", "scissors"] 
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @result = "tied"
+  elsif @comp_move == "scissors"
+   @result = "won"
+  elsif @comp_move = "paper"
+    @result = "lost"
+  end
+
+  erb(:rock)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"] 
+
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @result = "won"
+  elsif @comp_move == "scissors"
+    @result = "lost"
+  elsif @comp_move = "paper"
+    @result = "tied"
+  end
+ 
+  erb(:paper)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"] 
+
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @result = "lost"
+  elsif @comp_move == "scissors"
+    @result = "tied"
+  elsif @comp_move = "paper"
+    @result = "won"
+  end
+ 
+  erb(:scissors)
+end
+
